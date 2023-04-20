3fbfff8a0918d2844b9279e13de368e78f2417d6
Lyndley-Anne Naidoo <Lyndley-Anne.Naidoo@ekurhuleni.gov.za>
2023-04-20 17:55:00 +0200

    eight commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
+  
+  user: any[] = [];
+  loginUser ={
+  email: String,
+  password: String
+}
+login(){
+  this.user.push(this.loginUser);
+  localStorage.setItem('user', JSON.stringify(this.user));
+}
+  
 
 }
