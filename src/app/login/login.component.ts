a64989f67fbedd74524bdfe67a5ed3d450d474d3
Lyndley-Anne Naidoo <Lyndley-Anne.Naidoo@ekurhuleni.gov.za>
2023-04-24 14:08:03 +0200

    ninth commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,20 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  email: string = '';
+  password: string = '';
+  users: any[] = [];
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    let storedUsers = localStorage.getItem('registerUser');
+    if (storedUsers) {
+      this.users = JSON.parse(storedUsers);
+    }
+  }
+
+  login() {
+    let userExists = false;
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].email === this.email && this.users[i].password === this.password) {
+        userExists = true;
+        alert('Login Successful');
+        this.router.navigate(['./home']);
+        return;
+      }
+    }
+    if (!userExists) {
+      alert('User does not exist');
+      this.router.navigate(['./register']);
+    }
   }
   
-  user: any[] = [];
-  loginUser ={
-  email: String,
-  password: String
-}
-login(){
-  this.user.push(this.loginUser);
-  localStorage.setItem('user', JSON.stringify(this.user));
-}
-  
-
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  name: string = '';
+  surname: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  gender: string = '';
+  email: string = '';
+  cell: string = '';
+  question: string = '';
+  answer: string = '';
+ 
+  constructor() {
+    
   }
 
+  ngOnInit(): void {
+    let storedUsers = localStorage.getItem('registerUser');
+    if (storedUsers) {
+      this.registerUser = JSON.parse(storedUsers);
+    } else {
+      this.registerUser = [];
+    }
+  }
+
+  questions: string[] = ['What is your ID number?', 'what year did you finish school?'];
+  registerUser: any[] = [];
+
+  register() {
+    for(let i = 0; i < this.registerUser.length; i++){
+      if (this.registerUser[i].email === this.email){
+        alert("This User already exists");
+        return;
+      }
+    }
+    //if (!this.name || !this.surname || !this.password || !this.confirmPassword || !this.gender || !this.email || !this.cell || !this.question || !this.answer) {
+      //alert("Please fill in ALL fields!");
+     {
+      let user = {
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        cell: this.cell,
+        question: this.question,
+        answer: this.answer,
+        gender: this.gender,
+      };
+      this.registerUser.push(user);
+      localStorage.setItem('registerUser', JSON.stringify(this.registerUser));
+      alert("Registration successful!");
+      this.directToLogin();
+    }
+  }
+
+  directToLogin(){
+    document.location.href = "http://localhost:4200/login"
+  }
 }
